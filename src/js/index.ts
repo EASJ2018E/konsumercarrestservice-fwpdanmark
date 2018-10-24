518f965f36e1b266d463508dd6ebb2f3c8b7e383
Frederik Wulff Pedersen <fred957b@edu.easj.dk>
2018-10-24 11:25:43 +0200

    Færdig getall

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,8 +4,30 @@ import axios, {
 
     //http://rest-pele-easj-dk.azurewebsites.net/api/cars
 
-interface Icar {
+interface ICar {
     model: string;
     vendor: string;
     price: number;
+}
+
+let buttontag: HTMLButtonElement = document.getElementById("getAllButton") as HTMLButtonElement;
+buttontag.addEventListener("click",getAllCars);
+let divtag: HTMLDivElement = document.getElementById("content") as HTMLDivElement;
+
+function getAllCars(): void{
+    let uri :string = "http://rest-pele-easj-dk.azurewebsites.net/api/cars";
+
+    axios.get<ICar[]>(uri)
+    .then(function (responce:AxiosResponse<ICar[]>):void{
+
+        let result: string = "<ol>";
+        responce.data.forEach((car: ICar) => {
+            result += "<li>"+ car.model+" " + car.vendor +" " +car.price +"</li>"
+        });
+        result+= "</ol>"
+        divtag.innerHTML = result;
+    })
+    .catch(function (error: AxiosError): void{
+        divtag.innerHTML = error.message;
+    })
 }
