02d25a6ac00907b125ec228e006b5b9b113ae05d
Frederik Wulff Pedersen <fred957b@edu.easj.dk>
2018-10-24 13:48:23 +0200

    Delete + add

--- src/js/index.ts
+++ src/js/index.ts
@@ -13,16 +13,23 @@ interface ICar {
 let buttontag: HTMLButtonElement = document.getElementById("getAllButton") as HTMLButtonElement;
 buttontag.addEventListener("click",getAllCars);
 let divtag: HTMLDivElement = document.getElementById("content") as HTMLDivElement;
+let uri :string = "http://rest-pele-easj-dk.azurewebsites.net/api/cars";
 
 function getAllCars(): void{
-    let uri :string = "http://rest-pele-easj-dk.azurewebsites.net/api/cars";
+    
 
     axios.get<ICar[]>(uri)
     .then(function (responce:AxiosResponse<ICar[]>):void{
 
         let result: string = "<ol>";
         responce.data.forEach((car: ICar) => {
-            result += "<li>"+ car.model+" " + car.vendor +" " +car.price +"</li>"
+            if(car == null){
+                result += "<li> Null element</li>"
+            }
+            else{
+                result += "<li>"+ car.model+" " + car.vendor +" " +car.price +"</li>"
+            }
+            
         });
         result+= "</ol>"
         divtag.innerHTML = result;
@@ -30,4 +37,34 @@ function getAllCars(): void{
     .catch(function (error: AxiosError): void{
         divtag.innerHTML = error.message;
     })
+}
+
+let postButtonElement: HTMLButtonElement = document.getElementById("postButton") as HTMLButtonElement;
+postButtonElement.addEventListener("click", postCar);
+
+function postCar(): void{
+    let vendorField: HTMLInputElement = document.getElementById("inputVendor") as HTMLInputElement; 
+    let modelField: HTMLInputElement = document.getElementById("inputModel") as HTMLInputElement;
+    let priceField: HTMLInputElement = document.getElementById("inputPrice") as HTMLInputElement;
+
+    let myVendor: string = vendorField.value;
+    let myModel: string = modelField.value;
+    let myPrice: number = Number(priceField.value);
+
+    axios.post<ICar>(uri,{model:myModel, vendor:myVendor, price:myPrice})
+    .then((response: AxiosResponse) => {console.log(response.status +" " + response.statusText)})
+    .catch((error: AxiosError) => {console.log(error);} )
+}
+
+let deleteButtonElement: HTMLButtonElement = document.getElementById("deleteButton") as HTMLButtonElement
+deleteButtonElement.addEventListener("click",deleteCar);
+
+function deleteCar(): void{
+    let modelDeleteField: HTMLInputElement = document.getElementById("deleteCar") as HTMLInputElement
+    let newUri = "http://rest-pele-easj-dk.azurewebsites.net/api/cars/"+modelDeleteField.value;
+    axios.delete(newUri)
+    .then((response: AxiosResponse) => {console.log(response.status +" " + response.statusText)})
+    .catch((error: AxiosError) => {console.log(error);} )
+        
+    
 }
